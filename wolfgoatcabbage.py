7a4aab0166ae44c5bd9061e6884cd5f120c6c077
Angarag Gansukh <angaraggansukh@csu.fullerton.edu>
2023-09-26 13:51:16 -0700

    Create skeleton program

--- wolfgoatcabbage.py
+++ wolfgoatcabbage.py
@@ -3,14 +3,24 @@ from search import *
 
 class WolfGoatCabbage(Problem):
     def __init__(self, initial=[['F', 'W', 'C', 'G'], []], goal=[[], 'F']):
+        super().__init__(initial, goal)
 
-        super().__init__(self, initial, goal)
+    # return True if is the given state is a goal state
+    def goal_test(state):
+        pass
 
+    # returns the new state reached from the given state and the given action 
+    def result(state, action):
+        pass
+    
+    # returns a list of valid actions in the given state
+    def actions(state):
+        pass
 
 if __name__ == "__main__":
     wgc = WolfGoatCabbage()
     print(wgc.initial)
-    # solution = depth_first_graph_search(wgc).solution()
-    # print(solution)
-    # solution = breadth_first_graph_search(wgc).solution()
-    # print(solution)
+    solution = depth_first_graph_search(wgc).solution()
+    print(solution)
+    solution = breadth_first_graph_search(wgc).solution()
+    print(solution)
